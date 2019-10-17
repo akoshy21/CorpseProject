ac39a7241c098434611b217ada852026453e875f
akoshy21 <ak6429@nyu.edu>
2019-10-17 09:34:05 -0400

    small ragdoll tweaks

--- Corpse Project/Assets/AK Test Stuff/RagdollManager.cs
+++ Corpse Project/Assets/AK Test Stuff/RagdollManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class RagdollManager : MonoBehaviour
 {
+    public bool dead;
+    public static RagdollManager player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = this;
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            //transform.GetChild(i).gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            print("For loop: " + transform.GetChild(i));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dead)
+        {
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                //transform.GetChild(i).gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                transform.GetChild(i).gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                transform.GetChild(i).gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+                print("For loop: " + transform.GetChild(i));
+
+            }
+        }
     }
 }
