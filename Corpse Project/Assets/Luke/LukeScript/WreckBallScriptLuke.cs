604337f1e98ec2f5f42f6e8a06a90aa4fc9878fc
Luke Brockmann <lb3297@nyu.edu>
2019-10-17 09:40:16 -0400

    WreckingBall Tweaks
    Also added a new scene

--- Corpse Project/Assets/Luke/LukeScript/WreckBallScriptLuke.cs
+++ Corpse Project/Assets/Luke/LukeScript/WreckBallScriptLuke.cs
@@ -7,23 +7,56 @@ public class WreckBallScriptLuke : MonoBehaviour
 
     public Rigidbody2D rigbod;
 
-    public float initPush;
-
+    public float rePushx;
+    public float rePushy;
+    public float speedCap;
+    private Vector2 vel;
     public float normalPush;
+    public GameObject ball;
+
+    public Transform xPos;
     // Start is called before the first frame update
     void Start()
     {
-        rigbod.AddForce(new Vector2(initPush,0f), ForceMode2D.Impulse);
+        //rigbod.AddForce(new Vector2(initPush,0f), ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vel = rigbod.velocity;
+        if (vel.magnitude < speedCap)
+        {
+            if (ball.transform.position.x > xPos.transform.position.x)
+            {
+                rigbod.AddForce(new Vector2(-rePushx,rePushy), ForceMode2D.Impulse);
+            }
+            
+            if (ball.transform.position.x < xPos.transform.position.x)
+            {
+                rigbod.AddForce(new Vector2(rePushx,rePushy), ForceMode2D.Impulse);
+            }
+           
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player1"))
+        {
+           //Kill Player 1
+        }
+        
+        if(other.gameObject.CompareTag("Player2"))
+        {
+            //Kill Player 2
+        }
+
+       
     }
 }
