0d642a9f781fb604065d8ea74fb3879aaed4fb8c
Kate <kah703@nyu.edu>
2019-10-22 18:56:47 -0400

    Spike Prefab, Lethal Script, Sticky Limb, beginning of the weapon class, deleted an extra starter class that was out of the folders (possibliy due to a pull issue)

--- Assets/Scripts/Hazards/Lethal.cs
+++ Assets/Scripts/Hazards/Lethal.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Lethal : MonoBehaviour
 {
+    /*
+     * @author Kate Howell
+     */
+    
+    //this function is derived from a scirpt Carsen originally wrote on the Player Controller
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.transform.tag == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
-            //PlayerController pC = other.tr
+            PlayerController pC = other.transform.parent.GetComponentInChildren<PlayerController>();
+            if (pC != null && !pC.dead)
+            {
+                pC.Die();
+            }
+            
+            
         }
     }
+
+    
 }
