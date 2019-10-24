4a77be1321affe5406888952e862468952d62e21
crownella <35846857+crownella@users.noreply.github.com>
2019-10-24 01:24:12 -0400

    Added Goal Prefab, Spawn Prefab, Platform Prefab, Imported TextMeshPro and lots of fonts, Added end scene, Finished weapon class, Implemented a functioning Pistol, Added sprite to spikes, Added sprite to platform

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -2,19 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
     /*
      * @author Kate Howell
+     * 
+     * This class serves as a abstract class to build weapons off of
+     * 
+     * This class allows you to attack, and allows you to equip and unequip the weapon
      */
-    
-    public float damage;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField, Tooltip("If gun is equipped to a Player")]
+    public bool equipped;
+
+    public abstract void Attack();
+
+    //function equips and unequips weapon
+    public void Equip(GameObject player)
     {
-        
+        PlayerController controller = player.transform.parent.GetComponentInChildren<PlayerController>();
+        if (equipped)
+        {
+            equipped = false;
+            //unequip
+            this.gameObject.AddComponent<Rigidbody2D>();
+            this.transform.parent = null;
+            if(this.transform.parent != null)
+            {
+                throw new System.Exception("Weapon failed to unequip, parent != null");
+            }
+        }
+        else
+        {
+            equipped = true;
+            
+            
+
+            //Testing equip until player controller can be modified
+            GunTest GunTest = player.transform.parent.GetComponent<GunTest>();
+
+            if(GunTest.gunlocation == null)
+            {
+                throw new System.Exception("Weapon failed to Equip, gunLocation = null");
+            }
+
+            this.transform.SetParent(GunTest.gunlocation.transform);
+            this.transform.SetPositionAndRotation(GunTest.gunlocation.transform.position, GunTest.gunlocation.transform.rotation);
+
+            if (this.transform.parent == null)
+            {
+                throw new System.Exception("Weapon failed to Equip, parent = null");
+            }
+
+            Destroy(this.GetComponent<Rigidbody2D>());
+
+            GunTest.weaponEquipped = true;
+            GunTest.weapon = this;
+            if(GunTest.weapon == null)
+            {
+                throw new System.Exception("Weapon failed to Equip, GunTestweapon = null");
+            }
+        }
     }
 
-    
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!equipped)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Equip(collision.gameObject);
+            }
+        }
+            
+    }
+
 }
