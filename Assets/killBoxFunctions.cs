2c57b9e988b47598021cac2c68d4d27a881c3ef3
Luke Brockmann <lb3297@nyu.edu>
2019-10-24 15:48:35 -0400

    Kill box prefab added.

---
+++ Assets/killBoxFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killBoxFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController controller = other.transform.parent.gameObject.GetComponentInChildren<PlayerController>();
+            controller.Die();
+            Debug.Log("player fell out");
+        }
+    }
+}
