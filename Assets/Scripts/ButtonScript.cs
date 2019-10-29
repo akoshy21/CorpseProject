b157e8a02d70f3b0932fdd3aa750b726baf7759b
Luke Brockmann <lb3297@nyu.edu>
2019-10-29 16:44:15 -0400

    Imma be real I don't remember

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -7,40 +7,65 @@ public class ButtonScript : MonoBehaviour
 {
     public bool isLever;
     public bool buttonActive;
+    private List<GameObject> entryList;
+    public bool stayOn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        entryList = new List<GameObject>();  
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+       
+        if (entryList.Count > 0)
+        {
+            Debug.Log("List go");
+            if (isLever == false)
+            {
+                buttonActive = true;
+                Debug.Log("button message sent");
+            }
+
+            if (isLever)
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    buttonActive = true;
+                    Debug.Log("Lever messge sent");
+                }
+                else
+                {
+                    buttonActive = false;
+                }
+            }
+        }
+        else
+        {
+            buttonActive = false;
+        }
+
+     
+
         
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (isLever == false)
-        {
-            buttonActive = true;
-            Debug.Log("button message sent");
-        }
-
-        if (isLever)
-        {
-            if (Input.GetKey(KeyCode.E))
-            {
-                buttonActive = true;
-                Debug.Log("Lever messge sent");
-            }
-        }
         
-        Debug.Log("Button hit");
+     
+        entryList.Add(other.gameObject);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        buttonActive = false;
+      Debug.Log(other);
+        
+
+        entryList.Remove(other.gameObject);
     }
+
+   
 }
