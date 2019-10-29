a514c23e6e388769a70a2ace17cfbdf9ed46986b
Luke Brockmann <lb3297@nyu.edu>
2019-10-29 14:40:29 -0400

    I moved some of my files around Edited ball and glass level Made functioning button and lever script Annamria fucked with some sprites.

---
+++ Assets/Scripts/FanScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanScript : MonoBehaviour
+{
+    public GameObject rightTrigger;
+    public GameObject leftTrigger;
+
+    public bool pushLeft;
+
+    public bool pushRight;
+    // Start is called before the first frame update
+    
+    //Luke Brockmann
+    void Start()
+    {
+        
+        if (pushRight)
+        {
+            rightTrigger.SetActive(true);
+        }
+
+        if (pushLeft)
+        {
+            leftTrigger.SetActive(true);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FanpushScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class FanpushScript : MonoBehaviour
+{
+    public bool pushLeft;
+
+    public bool pushRight;
+    private bool pushing;
+    public float ticker;
+    private float tickerCap;
+    private bool tick;
+    public GameObject pushedPlayer;
+    public float pushForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tickerCap = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ticker = ticker + 1f * Time.deltaTime;
+        if (ticker > tickerCap)
+        {
+            ticker = 0;
+            tick = true;
+            Debug.Log("push");
+        }
+
+        if (ticker < tickerCap)
+        {
+            tick = false;
+        }
+
+        if (pushing)
+        {
+            if (pushLeft)
+            {
+                if (tick)
+                {
+                    pushedPlayer.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-pushForce,0.5f),ForceMode2D.Impulse);
+                }
+            }
+
+            
+            
+            if (pushRight)
+            {
+                if (tick)
+                {
+                    pushedPlayer.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(pushForce,0.5f),ForceMode2D.Impulse);
+                    Debug.Log("Pushing");
+                }
+            }
+        }
+    }
+    
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+       /* if (pushLeft)
+        {
+            if (tick)
+            {
+                //other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-pushForce,0.5f),ForceMode2D.Impulse);
+            }
+        }
+
+            
+            
+        if (pushRight)
+        {
+            if (tick)
+            {
+                other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(pushForce,0.5f),ForceMode2D.Impulse);
+                Debug.Log("Pushing");
+            }
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            pushing = true;
+            Debug.Log("Trigger Enter Procced");
+            pushedPlayer = other.gameObject;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerOne")||other.CompareTag("PlayerTwo"))
+        {
+            
+            pushing = false;
+        }
+    }
+}
