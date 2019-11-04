710c86d948b98fa5e6ebbbd739a37e40854dc1fb
crownella <35846857+crownella@users.noreply.github.com>
2019-11-04 00:28:29 -0500

    falling hazard also in progress

--- Assets/Scripts/Hazards/FallingHazard.cs
+++ Assets/Scripts/Hazards/FallingHazard.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class FallingHazard : MonoBehaviour
 {
     public ButtonScript Button;
+    public bool activated;
+    private RaycastHit2D[] groundCheckRay;
 
     private void Update()
     {
+        //physics
+        Ray2D groundCheck = new Ray2D(transform.position, Vector2.down);
+        groundCheckRay = Physics2D.RaycastAll(groundCheck.origin, groundCheck.direction, 0.22f);
+
         if (Button.buttonActive)
         {
-
+            
         }
     }
     void Fall()
