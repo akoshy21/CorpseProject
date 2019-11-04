f786391c5e91f7b23112393a773d08bdcbda01b9
crownella <35846857+crownella@users.noreply.github.com>
2019-11-03 23:36:19 -0500

    i guess i dont know how to make a bounce pad bc i havent been able to get this to work

---
+++ Assets/Scripts/Hazards/BouncePad.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    public float launchHeight;
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            print("Player");
+            GameObject current = collision.gameObject;
+            PlayerController controller = current.transform.parent.GetComponentInChildren<PlayerController>();
+            if (controller == null)
+            {
+                print("Null1");
+                current = current.transform.parent.gameObject;
+                controller = current.transform.parent.GetComponentInChildren<PlayerController>();
+            }
+            if (controller == null)
+            {
+                print("Null2");
+                current = current.transform.parent.gameObject;
+                controller = current.transform.parent.GetComponentInChildren<PlayerController>();
+            }
+            if (controller != null && !controller.dead)
+            {
+                print("Jump");
+                Rigidbody2D rigidbody = controller.GetComponent<Rigidbody2D>();
+                if(rigidbody != null)
+                {
+                    print("TheActualJump");
+                    rigidbody.AddForce(new Vector2(0, launchHeight), ForceMode2D.Impulse);
+                }
+            }
+
+        }
+        
+        if (collision.transform.parent.CompareTag("PlayerOne"))
+        {
+            PlayerController controller = collision.transform.parent.GetComponentInChildren<PlayerController>();
+            if (controller != null && !controller.dead)
+            {
+                Rigidbody2D rigidbody = controller.GetComponent<Rigidbody2D>();
+                if(rigidbody != null)
+                {
+                    rigidbody.AddForce(new Vector2(0, launchHeight), ForceMode2D.Impulse);
+                }
+            }
+        }
+        else if (collision.transform.parent.CompareTag("PlayerTwo"))
+        {
+            print("Trigger");
+            PlayerController controller = collision.transform.parent.GetComponentInChildren<PlayerController>();
+            if (controller != null && !controller.dead)
+            {
+                Rigidbody2D rigidbody = controller.GetComponent<Rigidbody2D>();
+                if (rigidbody != null)
+                {
+                    rigidbody.AddForce(new Vector2(0, launchHeight), ForceMode2D.Impulse);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Hazards/FallingHazard.cs
+++ Assets/Scripts/Hazards/FallingHazard.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FallingHazard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ButtonScript Button;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Button.buttonActive)
+        {
+
+        }
+    }
+    void Fall()
+    {
+
     }
 }
