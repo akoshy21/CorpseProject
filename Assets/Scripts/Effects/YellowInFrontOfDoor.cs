761fc818a72b9ba21126040cd3c0024e76a58d0e
Carsen Decker <cad631@nyu.edu>
2021-02-09 00:25:18 -0500

    idk

---
+++ Assets/Scripts/Effects/YellowInFrontOfDoor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// This script is niche af
+/// </summary>
+public class YellowInFrontOfDoor : MonoBehaviour
+{
+    public Color YellowColor;
+
+    private Color originalColor;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
+    }
+
+    void Update()
+    {
+        //Wait this doesn't work the doors ignore raycasts smh
+        RaycastHit2D[] behindCheck = Physics2D.RaycastAll(transform.position, Vector2.left);
+
+        bool foundDoor = false;
+        foreach (RaycastHit2D hit in behindCheck)
+        {
+            if (hit.collider.GetComponent<Goal>())
+            {
+                foundDoor = true;
+            }
+        }
+
+        if (foundDoor)
+            sr.color = YellowColor;
+        else
+            sr.color = originalColor;
+    }
+
+}
